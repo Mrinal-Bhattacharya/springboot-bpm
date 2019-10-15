beafb28b0400b95c64d6314488358bbef99b7d40
Mrinal Bhattacharya <mrinal.bhattacharya@boeing.com>
2019-10-15 20:01:58 +0530

    Inital commit

---
+++ src/main/java/com/springboot/bpm/demo/springbootbpm/SpringbootBpmApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bpm.demo.springbootbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBpmApplication.class, args);
+	}
+
+}
